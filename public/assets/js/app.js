e2f343a8f63bc7a4646c8b664e63fc11baddca60
Edgar Burelo <edgar.burelo@gmail.com>
2019-01-07 18:28:07 -0600

    finished 1

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -21,8 +21,6 @@ $(document).ready(()=>{
             location.reload();
             console.log("algo");
         });
-        
-        
     });
 });
 
--- server.js
+++ server.js
@@ -25,24 +25,19 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.get("/",(req,res) => {
     db.Article.find({}).then(dbArticle => {
-            //res.json(dbArticle);
-            res.render("index", dbArticle);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+        res.render("index", dbArticle);
+    }).catch(err => {
+        res.json(err);
+    });
 });
 
 app.get("/articles/:id",(req,res) => {
-    db.Article.findOne({ _id: req.params.id })
-        .populate("comment")
-        .then(dbArticle => {
-            console.log(dbArticle);
-            res.render("comment",dbArticle);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+    db.Article.findOne({ _id: req.params.id }).populate("comment").then(dbArticle => {
+        //console.log(dbArticle);
+        res.render("comment",dbArticle);
+    }).catch(err => {
+        res.json(err);
+    });
 });
 
 app.get("/scrape",(req,res) => {
@@ -76,35 +71,17 @@ app.get("/scrape",(req,res) => {
             };
         });
         res.send("Scrape Complete");
-
     });
 });
 
 app.post("/comment/:id", (req, res) => {
-    db.Comment.create(req.body)
-        .then(dbComment => {
-            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
-        }).then(function (dbArticle) {
-            // If we were able to successfully update an Article, send it back to the client
-            res.json(dbArticle);
-        })
-        .catch(function (err) {
-            // If an error occurred, send it to the client
-            res.json(err);
-        });
-});
-
-app.get("/prueba",(req,res) =>{
-    db.Article.find({}).then(dbArticle => {
-        //res.json(dbArticle);
+    db.Comment.create(req.body).then(dbComment => {
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
+    }).then(dbArticle => {
         res.json(dbArticle);
-    })
-        .catch(err => {
-            res.json(err);
-        });
+    }).catch(err => {
+        res.json(err);
+    });
 });
 
 app.listen(PORT, () => {
